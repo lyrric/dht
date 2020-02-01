9623fbe85517a0a6f1d26f77cace0f66b2e001a9
lyrric <xx20510@163.com>
2020-02-01 19:19:13 +0800

    种子信息存入mysql

--- dht-download/src/main/kotlin/com/github/lyrric/down/service/impl/TorrentServiceImpl.kt
+++ dht-download/src/main/kotlin/com/github/lyrric/down/service/impl/TorrentServiceImpl.kt
@@ -4,13 +4,13 @@ import com.github.lyrric.common.entity.TorrentInfo
 import com.github.lyrric.down.entity.Torrent
 import com.github.lyrric.down.mapper.TorrentMapper
 import com.github.lyrric.down.service.TorrentService
-import lombok.extern.slf4j.Slf4j
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.cloud.stream.annotation.StreamListener
 import org.springframework.stereotype.Service
 import tk.mybatis.mapper.weekend.Weekend
 import java.util.*
+import java.util.concurrent.Semaphore
 import javax.annotation.Resource
 
 @Service
@@ -19,20 +19,13 @@ class TorrentServiceImpl : TorrentService{
     @Resource
     private lateinit var torrentMapper: TorrentMapper
 
-    private val torrents:LinkedList<Torrent> = LinkedList();
+    private val torrents = LinkedList<Torrent>()
+
 
     private val log:Logger = LoggerFactory.getLogger(this.javaClass)
 
     @StreamListener("torrent-message")
     override fun torrentMessageIn(torrentInfo: TorrentInfo) {
-        log.info("torrentMessageIn---------------{}, size: {}", torrentInfo.infoHash, torrents.size)
-        val weekend:Weekend<Torrent> = Weekend(Torrent::class.java)
-        weekend.weekendCriteria()
-                .andEqualTo(Torrent::getInfoHash.name, torrentInfo.infoHash)
-        if(torrentMapper.selectCountByExample(weekend) > 0){
-            log.info(" torrentMapper exist in db")
-            return
-        }
         val torrent = Torrent();
         torrent.infoHash = torrentInfo.infoHash
         torrent.fileName = torrentInfo.fileName
@@ -41,14 +34,27 @@ class TorrentServiceImpl : TorrentService{
         torrent.files = torrentInfo.files
         torrent.torrentCreateTime = Date(torrentInfo.createDate)
         torrent.addTime = Date()
+        sync(torrent)
+    }
+
+    @Synchronized
+    fun sync(torrent: Torrent){
+        log.info("torrentMessageIn---------------{}, size: {}", torrent.infoHash, torrents.size)
+        val weekend:Weekend<Torrent> = Weekend(Torrent::class.java)
+        weekend.weekendCriteria()
+                .andEqualTo(Torrent::getInfoHash.name, torrent.infoHash)
+        if(torrentMapper.selectCountByExample(weekend) > 0){
+            log.info(" torrentMapper exist in db")
+            return
+        }
         synchronized(this){
             if(torrents.stream().anyMatch{t->t.infoHash == torrent.infoHash} ){
                 //判断列表中是否有相同hash，几率小，但是还是要避免，否则批量插入会失败
                 log.info(" torrentMapper exist in list")
                 return
             }
-            torrents.addLast(torrent)
             log.info(" torrents.addLast(torrent)")
+            torrents.add(torrent)
             //每满50个添加进数据库
             if(torrents.size >= 50){
                 log.info(" torrentMapper.insertList(torrents)---------------")
