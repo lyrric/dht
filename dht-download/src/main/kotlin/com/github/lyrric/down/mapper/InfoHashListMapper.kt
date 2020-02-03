6f653449aa5408b60f9ae406e9a2a1624f6b5918
lyrric <xx20510@163.com>
2020-02-03 10:51:51 +0800

    数据库保存hash，

--- dht-download/src/main/kotlin/com/github/lyrric/down/mapper/InfoHashListMapper.kt
+++ dht-download/src/main/kotlin/com/github/lyrric/down/mapper/InfoHashListMapper.kt
@@ -4,9 +4,4 @@ import com.github.lyrric.down.core.BaseMapper
 import com.github.lyrric.down.entity.InfoHashList
 import org.apache.ibatis.annotations.Select
 
-interface InfoHashListMapper : BaseMapper<InfoHashList?>{
-
-
-    @Select("select count(1) from info_hash_list where info_hash = #{infoHash}")
-    fun selectCount(infoHash:String):Int
-}
+interface InfoHashListMapper : BaseMapper<InfoHashList?>
--- dht-download/src/main/kotlin/com/github/lyrric/down/mapper/TorrentMapper.kt
+++ dht-download/src/main/kotlin/com/github/lyrric/down/mapper/TorrentMapper.kt
@@ -4,8 +4,4 @@ import com.github.lyrric.down.core.BaseMapper
 import com.github.lyrric.down.entity.Torrent
 import org.apache.ibatis.annotations.Select
 
-interface TorrentMapper : BaseMapper<Torrent?>{
-
-    @Select("select count(1) from info_hash where info_hash")
-    fun selectCount():Int
-}
+interface TorrentMapper : BaseMapper<Torrent?>
