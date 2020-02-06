b41a3bf2f1f246fb5094b6c617e7d4e6372565b3
lyrric <xx20510@163.com>
2020-02-06 22:47:06 +0800

    修改test

--- dht-web/src/main/kotlin/com/github/lyrric/web/service/impl/DHTServiceImpl.kt
+++ dht-web/src/main/kotlin/com/github/lyrric/web/service/impl/DHTServiceImpl.kt
@@ -3,6 +3,7 @@ package com.github.lyrric.web.service.impl
 import com.github.lyrric.common.entity.Node
 import com.github.lyrric.common.entity.Tree
 import com.github.lyrric.common.util.JSONUtil
+import com.github.lyrric.common.util.StringUtil
 import com.github.lyrric.web.entity.Torrent
 import com.github.lyrric.web.entity.Torrentt
 import com.github.lyrric.web.mapper.TorrentMapper
@@ -10,9 +11,11 @@ import com.github.lyrric.web.mapper.TorrentStatMapper
 import com.github.lyrric.web.mapper.TorrenttMapper
 import com.github.lyrric.web.service.DHTService
 import com.github.pagehelper.PageHelper
+import org.apache.ibatis.session.RowBounds
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import org.springframework.util.StringUtils
 import tk.mybatis.mapper.weekend.Weekend
 import javax.annotation.Resource
 
@@ -30,34 +33,38 @@ class DHTServiceImpl:DHTService {
 
     override fun test(){
         val weekend:Weekend<Torrent> = Weekend<Torrent>(Torrent::class.java)
-        weekend.weekendCriteria()
-                .andIsNotNull(Torrent::files.name)
-        weekend.orderBy(Torrent::addTime.name).asc()
-        var pageNum = 15
-        val pageSize = 100
+        weekend.orderBy("id").asc()
+        //461590
+        var offset = 0
+        val pageSize = 1000
         while(true){
-            val page = PageHelper.startPage<Torrent>(pageNum++, pageSize).doSelectPage<Torrent>{torrentMapper.selectByExample(weekend)}
+            val row = RowBounds(offset, pageSize)
+            val page = torrentMapper.selectByExampleAndRowBounds(weekend,row);
             if(page.isEmpty())break
-            val data = ArrayList<Torrentt>(100);
-            page.stream().forEach { torrent->
+            val data = ArrayList<Torrentt>(1000);
+            page.forEach { torrent->
                 run {
-                    val json = torrent.files
-                    val tree = JSONUtil.parseObject(json, Tree::class.java)
-                    var nodes = tree.leafList()
-                    torrent.files = JSONUtil.toJSONString(nodes)
-                            .replace("\"filename\"", "\"fileName\"")
-                            .replace("\"filesize\"", "\"fileSize\"")
+                    val json = torrent?.files
                     val torrentt = Torrentt()
-                    torrentt.infoHash = torrent.infoHash
-                    torrentt.fileName = torrent.fileName
-                    torrentt.fileSize = torrent.fileSize
-                    torrentt.files = torrent.files
-                    torrentt.addTime = torrent.addTime
+                    if(!StringUtils.isEmpty(json)){
+                        val tree = JSONUtil.parseObject(json, Tree::class.java)
+                        val nodes = tree.leafList()
+                        torrent?.files = JSONUtil.toJSONString(nodes)
+                                .replace("\"filename\"", "\"fileName\"")
+                                .replace("\"filesize\"", "\"fileSize\"")
+                        torrentt.files = torrent?.files
+                    }
+
+                    torrentt.infoHash = torrent?.infoHash
+                    torrentt.fileName = torrent?.fileName
+                    torrentt.fileSize = torrent?.fileSize
+                    torrentt.addTime = torrent?.addTime
                     data.add(torrentt)
                 }
             }
             torrenttMapper.insertList(data)
-            log.info("pageNum:{}", pageNum)
+            log.info("offset:{}", offset)
+            offset+=pageSize
         }
     }
 }
