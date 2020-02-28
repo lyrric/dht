1904ffd922ae297ed0a6a1f08926956557486713
wangxiaodong <xx20510@163.com>
2020-02-28 10:20:43 +0800

    修改计数方式

--- dht-server/src/main/kotlin/com/github/lyrric/server/netty/DHTServer.java
+++ dht-server/src/main/kotlin/com/github/lyrric/server/netty/DHTServer.java
@@ -82,27 +82,19 @@ public class DHTServer {
 	 * @param packet
 	 */
 	public void  sendKRPCWithLimit(DatagramPacket packet) {
-		long time = System.currentTimeMillis()/1000;
-		if(time == now){
-			if(secondSend >= sendLimit){
-				return;
-			}
-			secondSend++;
-		}else{
-			if((time % 10) == 0 && secondSend != 0){
-				log.info("本秒共发送:{} 次请求", secondSend);
-			}
-			secondSend = 0;
-			now = time;
+		if(count()){
+			return;
 		}
  		sendKRPC(packet);
 	}
+
 
 	/**
 	 * 发送get_peers，不限制发送频率
 	 * @param infoHash
 	 */
 	public void sendGetPeers(byte[] infoHash, InetSocketAddress address, String transactionId) {
+		count();
 		HashMap<String, Object> map = new HashMap<>(5);
 		map.put("info_hash", infoHash);
 		map.put("id", NetworkUtil.SELF_NODE_ID);
@@ -125,5 +117,26 @@ public class DHTServer {
 				log.info("write and flush msg exception. host {}, err msg:[{}]", packet.sender().getHostString(), e);
 			}
 		}
+	}
+
+	/**
+	 * 计数
+	 * @return
+	 */
+	private boolean count(){
+		long time = System.currentTimeMillis()/1000;
+		if(time == now){
+			if(secondSend >= sendLimit){
+				return true;
+			}
+			secondSend++;
+		}else{
+			if((time % 10) == 0 && secondSend != 0){
+				log.info("本秒共发送:{} 次请求", secondSend);
+			}
+			secondSend = 0;
+			now = time;
+		}
+		return false;
 	}
 }
